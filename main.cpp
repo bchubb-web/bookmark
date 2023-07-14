39fd7028f5a27006cc4eab3f6d6dc21d94a39199
bchubb <bchubb@Ubuntu.myguest.virtualbox.org>
2023-07-15 00:33:59 +0100

    getting files in directory

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include <string>
+#include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <filesystem>
-namespace fs = std::filesystem;
+#include <string>
 
 int main() {
-    std::string path = "./";
-    for (const auto & entry : fs::directory_iterator(path))
-        std::cout << entry.path << std::endl;
+    int totalSize;
+    std::string path = std::filesystem::current_path();
+    std::cout << path << std::endl << "===========================" << std::endl;
+    for (const auto & entry : std::filesystem::directory_iterator(path)) {
+        if (!entry.is_directory()){
+            std::cout << entry.path().string() << " - " <<  entry.file_size() << std::endl;
+        }
+        else {
+            std::cout << entry.path().string() << " - DIR" << std::endl;
+        }
+    }
 }
